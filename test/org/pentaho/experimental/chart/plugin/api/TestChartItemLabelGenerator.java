283308979fc9124166a1ff11e847a199dd7959ac
rhasija <rhasija@pentaho.com>
2008-04-03 14:28:47 +0000

    Created test case for ChartItemLabelGenerator.
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-commons/trunk/pentaho-chartbeans@22824 5d404267-db13-0410-8476-b1f6b398e63a

---
+++ test/org/pentaho/experimental/chart/plugin/api/TestChartItemLabelGenerator.java
@@ -0,0 +1,69 @@
+package org.pentaho.experimental.chart.plugin.api;
+
+
+import junit.framework.TestCase;
+
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.resourceloader.ResourceException;
+import org.junit.After;
+import org.junit.Before;
+import org.pentaho.experimental.chart.ChartDocumentContext;
+import org.pentaho.experimental.chart.ChartFactory;
+import org.pentaho.experimental.chart.core.ChartDocument;
+import org.pentaho.experimental.chart.core.ChartElement;
+import org.pentaho.experimental.chart.data.ChartTableModel;
+import org.pentaho.experimental.chart.plugin.jfreechart.utils.JFreeChartUtils;
+
+public class TestChartItemLabelGenerator extends TestCase {
+
+  private static final String ROW_NAME = "row-name"; //$NON-NLS-1$
+  
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+  
+  public void testNulls() throws ResourceException {
+    ChartDocumentContext cdc = ChartFactory.generateChart(getClass().getResource("ItemLabelTest1.xml")); //$NON-NLS-1$
+    ChartDocument cd = cdc.getChartDocument();
+    assertNotNull(cd);
+    if (cd == null) {
+      fail("A null document should never be returned"); //$NON-NLS-1$
+    }
+
+    ChartElement[] seriesElements = cd.getRootElement().findChildrenByName("series"); //$NON-NLS-1$
+    ChartTableModel chartData = createChartTableModel(null);
+    ChartItemLabelGenerator labelGen = new ChartItemLabelGenerator(seriesElements, chartData);
+    assertNotNull(labelGen);
+    
+    Object[][] dataArray = {{null, null, null}, {null, 40, 35}, {null, 35, 86}};
+    chartData = createChartTableModel(dataArray);
+    labelGen = new ChartItemLabelGenerator(seriesElements, chartData);
+    assertNotNull(labelGen);
+    
+    CategoryDataset categoryDataset = JFreeChartUtils.createDefaultCategoryDataset(chartData);
+    
+    for(int i=0; i< dataArray.length; i++) {
+      for (int j=0; j<dataArray[i].length; j++) {
+        labelGen.generateLabel(categoryDataset, i, j);    
+      }
+    }
+  }
+  
+  private ChartTableModel createChartTableModel(Object[][] dataArray) {
+    ChartTableModel data = new ChartTableModel();
+    data.setData(dataArray);
+    data.setColumnName(0, "budget"); //$NON-NLS-1$
+    data.setColumnName(1, "sales"); //$NON-NLS-1$
+    data.setColumnName(2, "forecast"); //$NON-NLS-1$
+
+    data.setRowMetadata(0, ROW_NAME, "Jan"); //$NON-NLS-1$
+    data.setRowMetadata(1, ROW_NAME, "Feb"); //$NON-NLS-1$
+    data.setRowMetadata(2, ROW_NAME, "Mar"); //$NON-NLS-1$
+
+    return data;
+  }
+}
