1e34978e9d9e33389253aed19df24e05bd0ce5ea
Thomas Morgner <tmorgner@pentaho.com>
2008-08-25 16:32:54 +0000

    Added IntelliJ module-definitions to all projects.
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-commons/trunk/pentaho-chartbeans@31266 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/experimental/chart/plugin/jfreechart/chart/bar/JFreeCylinderBarChartGenerator.java
+++ src/org/pentaho/experimental/chart/plugin/jfreechart/chart/bar/JFreeCylinderBarChartGenerator.java
@@ -4,27 +4,23 @@ import org.jfree.chart.JFreeChart;
 import org.pentaho.experimental.chart.ChartDocumentContext;
 import org.pentaho.experimental.chart.data.ChartTableModel;
 import org.pentaho.experimental.chart.plugin.jfreechart.utils.CylinderRenderer;
-import org.pentaho.experimental.chart.plugin.jfreechart.chart.bar.JFreeBarChartGenerator;
-import org.pentaho.experimental.chart.plugin.jfreechart.chart.bar.JFreeBarChartTypes;
 
 /**
- * Generates cylinder bar chart.
- * </p>
- * Author: Ravi Hasija
- * Date: May 14, 2008
- * Time: 1:33:56 PM
+ * Generates cylinder bar chart. </p> Author: Ravi Hasija Date: May 14, 2008 Time: 1:33:56 PM
  */
-public class JFreeCylinderBarChartGenerator extends JFreeBarChartGenerator {
+public class JFreeCylinderBarChartGenerator extends JFreeBarChartGenerator
+{
 
   /**
-   * Creates cylinder bar chart and creates range axis for it.
-   * </p>
+   * Creates cylinder bar chart and creates range axis for it. </p>
+   *
    * @param chartDocContext Current chart's document context
-   * @param data Current chart data
+   * @param data            Current chart data
    * @return Returns JFreeChart object that is a layered bar chart.
    */
   public JFreeChart createChart(final ChartDocumentContext chartDocContext,
-                                final ChartTableModel data) {
+                                final ChartTableModel data)
+  {
     final JFreeChart chart = createChart(chartDocContext, data, JFreeBarChartTypes.DEFAULT);
     final CylinderRenderer renderer = new CylinderRenderer();
     chart.getCategoryPlot().setRenderer(renderer);
@@ -32,7 +28,7 @@ public class JFreeCylinderBarChartGenerator extends JFreeBarChartGenerator {
      * Doing all the render stuff and then off to create range axis.
      * NOTE: The chart object will be updated.
      */
-    createRangeAxis(chartDocContext, data, chart);    
+    createRangeAxis(chartDocContext, data, chart);
     return chart;
   }
 }
