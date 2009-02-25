59f4e2b2df9147385a3623e529a42d117d99cec6
Angelo Rodriguez <arodriguez@pentaho.com>
2009-02-25 15:51:58 +0000

    PPP-1689
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-commons/pentaho-chartbeans@36560 5d404267-db13-0410-8476-b1f6b398e63a

--- trunk/src/org/pentaho/chart/model/DialPlot.java
+++ trunk/src/org/pentaho/chart/model/DialPlot.java
@@ -7,10 +7,10 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class DialPlot extends Plot implements Serializable {
-  public static class DialRange implements Serializable {
+  public static class DialRange implements Serializable, Comparable<DialRange> {
     Integer color;
-    Number minValue;
-    Number maxValue;
+    Number minValue = 0;
+    Number maxValue = 0;
 
     public DialRange() {  
     }
@@ -53,18 +53,68 @@ public class DialPlot extends Plot implements Serializable {
       this.maxValue = maxValue;
     }
 
+    public int compareTo(DialRange o) {
+      int comparison = 0;
+      
+      double thisValue = minValue.doubleValue();
+      double thatValue = o.minValue.doubleValue();
+      if (thisValue < thatValue) {
+        comparison = -1;
+      } else if (thisValue > thatValue) {
+        comparison = 1;
+      }
+      if (comparison == 0) {
+        thisValue = maxValue.doubleValue();
+        thatValue = o.maxValue.doubleValue();
+        if (thisValue < thatValue) {
+          comparison = -1;
+        } else if (thisValue > thatValue) {
+          comparison = 1;
+        }
+      }
+      return comparison;
+    }
+
+    public boolean equals(Object obj) {
+      boolean result = false;
+      if (obj instanceof DialRange) {
+        DialRange otherRange = (DialRange)obj;
+        result = minValue.equals(otherRange.minValue) && maxValue.equals(otherRange.maxValue);
+      }
+      return result;
+    }
+
   }
   
   // Really need a sorted set. But gwt 1.5.2 can't serialize sorted sets.
-  List<DialRange> divisions = new ArrayList<DialRange>();
+  ArrayList<DialRange> divisions = new ArrayList<DialRange>();
 
-  public SortedSet<DialRange> getDivisions() {
+  public SortedSet<DialRange> getRanges() {
     return new TreeSet<DialRange>(divisions);
   }
-
-  public void setDivisions(SortedSet<DialRange> divisions) {
-    divisions.clear();
-    divisions.addAll(divisions);
+  
+  public void addRange(DialRange dialRange) {
+    SortedSet<DialRange> ranges = getRanges();
+    if (!divisions.contains(dialRange)) {
+      ranges.add(dialRange);
+      divisions.clear();
+      divisions.addAll(ranges);
+      int index = divisions.indexOf(dialRange);
+      if (index > 0) {
+        if (divisions.get(index - 1).getMaxValue().doubleValue() > dialRange.getMinValue().doubleValue()) {
+          divisions.get(index - 1).setMaxValue(dialRange.getMinValue());
+        }
+      }
+      if (index < divisions.size() - 1) {
+        if (divisions.get(index + 1).getMinValue().doubleValue() < dialRange.getMaxValue().doubleValue()) {
+          divisions.get(index + 1).setMinValue(dialRange.getMaxValue());
+        }
+      }
+    }    
   }
-
+  
+  public void removeRange(DialRange dialRange) {
+    divisions.remove(dialRange);
+  }
+  
 }
