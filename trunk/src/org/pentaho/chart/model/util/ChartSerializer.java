474f3eb67808803258adc18c9d17375a4fdfc2df
Curtis Boyden <cboyden@pentaho.com>
2009-03-01 05:16:23 +0000

    [PPP-1685] Changed Theme to an XStream attribute
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-commons/pentaho-chartbeans@36639 5d404267-db13-0410-8476-b1f6b398e63a

--- trunk/src/org/pentaho/chart/model/util/ChartSerializer.java
+++ trunk/src/org/pentaho/chart/model/util/ChartSerializer.java
@@ -2,7 +2,6 @@ package org.pentaho.chart.model.util;
 
 import org.pentaho.chart.model.ChartModel;
 import org.pentaho.chart.model.Graph;
-import org.pentaho.chart.model.ChartModel.ChartTheme;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
@@ -11,11 +10,12 @@ public class ChartSerializer {
   private static XStream xstreamWriter = new XStream(new JettisonMappedXmlDriver());
   private static XStream xstreamReader = new XStream(new JettisonMappedXmlDriver());
   static{
-    xstreamWriter.alias("ChartModel", ChartModel.class);
-    xstreamReader.alias("ChartModel", ChartModel.class);
+    xstreamWriter.alias("ChartModel", ChartModel.class); //$NON-NLS-1$
+    xstreamReader.alias("ChartModel", ChartModel.class); //$NON-NLS-1$
     xstreamWriter.setMode(XStream.NO_REFERENCES);
     xstreamReader.setMode(XStream.NO_REFERENCES);
-    xstreamWriter.useAttributeFor(Graph.class, "categoryAxisLabel");
+    xstreamWriter.useAttributeFor(ChartModel.class, "theme"); //$NON-NLS-1$
+    xstreamWriter.useAttributeFor(Graph.class, "categoryAxisLabel"); //$NON-NLS-1$
     
   }
   public static String serialize(ChartModel model){
