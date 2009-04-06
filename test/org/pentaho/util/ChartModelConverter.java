4911ceab62eb8770e7988a1837488d2889473058
Angelo Rodriguez <arodriguez@pentaho.com>
2009-04-06 21:21:06 +0000

    Latest version
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-commons/pentaho-chartbeans/branches/PPP-1741@37133 5d404267-db13-0410-8476-b1f6b398e63a

--- test/org/pentaho/util/ChartModelConverter.java
+++ test/org/pentaho/util/ChartModelConverter.java
@@ -1,6 +1,18 @@
 package org.pentaho.util;
 
+import org.pentaho.chart.model.AreaPlot;
+import org.pentaho.chart.model.BarPlot;
 import org.pentaho.chart.model.ChartModel;
+import org.pentaho.chart.model.CssStyle;
+import org.pentaho.chart.model.GraphPlot;
+import org.pentaho.chart.model.LinePlot;
+import org.pentaho.chart.model.Palette;
+import org.pentaho.chart.model.PiePlot;
+import org.pentaho.chart.model.Plot;
+import org.pentaho.chart.model.BarPlot.BarPlotFlavor;
+import org.pentaho.chart.model.LinePlot.LinePlotFlavor;
+import org.pentaho.chart.model.Plot.Orientation;
+import org.pentaho.chart.model.Theme.ChartTheme;
 
 import com.thoughtworks.xstream.converters.Converter;
 import com.thoughtworks.xstream.converters.MarshallingContext;
@@ -47,16 +59,147 @@ public class ChartModelConverter implements Converter {
   public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
     
     ChartModel chartModel = new ChartModel();
-    String nodeName = reader.getNodeName();
-    reader.moveDown();
-    nodeName = reader.getNodeName();
-    reader.moveUp();
-    reader.moveDown();
-    nodeName = reader.getNodeName();
-    reader.moveDown();
+    String attribute = reader.getAttribute("chartEngine");
+    if (attribute != null) {
+      try {
+        chartModel.setChartEngine(Integer.parseInt(attribute));
+      } catch (Exception e) {
+        // Do nothing
+      }
+    }
+    
+    attribute = reader.getAttribute("theme");
+    if (attribute != null) {
+      ;
+      try {
+        chartModel.setTheme(Enum.valueOf(ChartTheme.class, attribute.toUpperCase()));
+      } catch (Exception e) {
+        // Do nothing
+      }
+    }
+    
+    while (reader.hasMoreChildren()) {
+      reader.moveDown();
+      if (reader.getNodeName().equals("title")) {
+        String title = reader.getValue();
+        if (title != null) {
+          chartModel.getTitle().setText(title);
+        } 
+        String cssStyle = reader.getAttribute("style");
+        if (cssStyle != null) {
+          chartModel.getTitle().getStyle().setStyleString(cssStyle);
+        }
+      } else if (reader.getNodeName().equals("legend")) {
+        chartModel.getLegend().setVisible(true);
+        String cssStyle = reader.getAttribute("style");
+        if (cssStyle != null) {
+          chartModel.getLegend().getStyle().setStyleString(cssStyle);
+        }
+      } else if (reader.getNodeName().equals("barPlot")
+          || reader.getNodeName().equals("linePlot") 
+          || reader.getNodeName().equals("areaPlot")
+          || reader.getNodeName().equals("piePlot")) {        
+        chartModel.setPlot(createPlot(reader));
+      }
+      reader.moveUp();
+    }
+      	
     return chartModel;
   }
 
+  private Plot createPlot(HierarchicalStreamReader reader) {
+    Plot plot = null;
+    if (reader.getNodeName().equals("barPlot")) {
+      BarPlot barPlot = new BarPlot();       
+      String flavor = reader.getAttribute("flavor");
+      if (flavor != null) {
+        try {
+          barPlot.setFlavor(Enum.valueOf(BarPlotFlavor.class, flavor.toUpperCase()));
+        } catch (Exception ex) {
+          // Do nothing, we'll stay with the default.
+        }
+      }
+      plot = barPlot;
+    } else if (reader.getNodeName().equals("linePlot")) {
+      LinePlot linePlot = new LinePlot();
+      String flavor = reader.getAttribute("flavor");
+      if (flavor != null) {
+        try {
+          linePlot.setFlavor(Enum.valueOf(LinePlotFlavor.class, flavor.toUpperCase()));
+        } catch (Exception ex) {
+          // Do nothing, we'll stay with the default.
+        }
+      }
+      plot = linePlot;
+    } else if (reader.getNodeName().equals("areaPlot")) {
+      plot = new AreaPlot();
+    } else if (reader.getNodeName().equals("piePlot")) {
+      PiePlot piePlot = new PiePlot();
+      piePlot.setAnimate(Boolean.parseBoolean(reader.getAttribute("animate")));
+      plot = piePlot;
+    }    
+    
+    String orientation = reader.getAttribute("orientation");
+    if (orientation != null) {
+      try {
+        plot.setOrientation(Enum.valueOf(Orientation.class, orientation.toUpperCase()));
+      } catch (Exception ex) {
+        // Do nothing, we'll stay with the default.
+      }
+    }
+    
+    String cssStyle = reader.getAttribute("style");
+    if (cssStyle != null) {
+      plot.getStyle().setStyleString(cssStyle);
+    }
+    
+    while (reader.hasMoreChildren()) {
+      reader.moveDown();
+      if (reader.getNodeName().equals("palette")) {
+        CssStyle paintStyle = new CssStyle();
+        Palette palette = new Palette();
+        while (reader.hasMoreChildren()) {
+          reader.moveDown();
+          if (reader.getNodeName().equals("paint")) {
+            cssStyle = reader.getAttribute("style");
+            if (cssStyle != null) {
+              paintStyle.setStyleString(cssStyle);
+              Integer color = paintStyle.getColor();
+              if (color != null) {
+                palette.add(color);
+              }
+            }
+          }
+          reader.moveUp();
+        }
+        if (palette.size() > 0) {
+          plot.setPalette(palette);
+        }
+      }
+      if (reader.getNodeName().equals("xAxisLabel") && (plot instanceof GraphPlot)) {
+        String title = reader.getValue();
+        if (title != null) {
+          ((GraphPlot)plot).getXAxisLabel().setText(title);
+        } 
+        cssStyle = reader.getAttribute("style");
+        if (cssStyle != null) {
+          ((GraphPlot)plot).getXAxisLabel().getStyle().setStyleString(cssStyle);
+        }
+      } else if (reader.getNodeName().equals("yAxisLabel") && (plot instanceof GraphPlot)) {
+        String title = reader.getValue();
+        if (title != null) {
+          ((GraphPlot)plot).getYAxisLabel().setText(title);
+        } 
+        cssStyle = reader.getAttribute("style");
+        if (cssStyle != null) {
+          ((GraphPlot)plot).getYAxisLabel().getStyle().setStyleString(cssStyle);
+        }
+      }  
+      reader.moveUp();
+    }
+    return plot;
+  }
+  
   public boolean canConvert(Class clazz) {
     return clazz.equals(ChartModel.class);
   }
