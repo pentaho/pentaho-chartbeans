161889cb6a65c4a14348d83935c4a44835aa5135
Angelo Rodriguez <arodriguez@pentaho.com>
2009-04-07 17:58:07 +0000

    PPP-1741
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-commons/pentaho-chartbeans/branches/PPP-1741@37157 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/chart/model/util/ChartSerializer.java
+++ src/org/pentaho/chart/model/util/ChartSerializer.java
@@ -1,47 +1,111 @@
 package org.pentaho.chart.model.util;
 
+import org.pentaho.chart.model.ChartLegend;
 import org.pentaho.chart.model.ChartModel;
+import org.pentaho.chart.model.CssStyle;
 import org.pentaho.chart.model.GraphPlot;
+import org.pentaho.chart.model.PiePlot;
+import org.pentaho.chart.model.BarPlot.BarPlotFlavor;
+import org.pentaho.chart.model.LinePlot.LinePlotFlavor;
+import org.pentaho.chart.model.Plot.Orientation;
+import org.pentaho.platform.plugin.action.chartbeans.ChartDataDefinition;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
-import org.pentaho.platform.plugin.action.chartbeans.ChartDataDefinition;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 public class ChartSerializer {
-  private static XStream chartWriter = new XStream(new JettisonMappedXmlDriver());
+  public enum ChartSerializationFormat {JSON, XML};
   
-  private static XStream chartDefWriter = new XStream(new JettisonMappedXmlDriver());
+  private static XStream jsonChartWriter = new XStream(new JettisonMappedXmlDriver());  
+  private static XStream jsonChartDefWriter = new XStream(new JettisonMappedXmlDriver());
+  private static XStream xmlChartWriter = new XStream(new DomDriver());  
+  private static XStream xmlChartDefWriter = new XStream(new DomDriver());
   
   static{
-    chartWriter.alias("ChartModel", ChartModel.class); //$NON-NLS-1$
+    jsonChartWriter.setMode(XStream.NO_REFERENCES);
+    jsonChartWriter.alias("chartModel", ChartModel.class);
+    jsonChartWriter.useAttributeFor(CssStyle.class); //$NON-NLS-1$
+    jsonChartWriter.useAttributeFor(Orientation.class); //$NON-NLS-1$
+    jsonChartWriter.useAttributeFor(LinePlotFlavor.class); //$NON-NLS-1$
+    jsonChartWriter.useAttributeFor(BarPlotFlavor.class); //$NON-NLS-1$
+    jsonChartWriter.registerConverter(new CssStylesConverter());
+    jsonChartWriter.omitField(ChartLegend.class, "visible");
+    jsonChartWriter.registerConverter(new StyledTextConverter());
+    jsonChartWriter.registerConverter(new PaletteConverter());
+    jsonChartWriter.registerConverter(new ChartModelConverter());
+    jsonChartWriter.useAttributeFor(PiePlot.class, "animate"); //$NON-NLS-1$
     
-    chartWriter.setMode(XStream.NO_REFERENCES);
-    chartWriter.useAttributeFor(GraphPlot.class, "categoryAxisLabel"); //$NON-NLS-1$
-    chartWriter.useAttributeFor(ChartModel.class, "theme"); //$NON-NLS-1$
-    chartWriter.useAttributeFor(ChartModel.class, "chartEngine"); //$NON-NLS-1$
-    chartWriter.useAttributeFor(ChartModel.class, "animate"); //$NON-NLS-1$
+    xmlChartWriter.setMode(XStream.NO_REFERENCES);
+    xmlChartWriter.alias("chartModel", ChartModel.class);
+    xmlChartWriter.useAttributeFor(CssStyle.class); //$NON-NLS-1$
+    xmlChartWriter.useAttributeFor(Orientation.class); //$NON-NLS-1$
+    xmlChartWriter.useAttributeFor(LinePlotFlavor.class); //$NON-NLS-1$
+    xmlChartWriter.useAttributeFor(BarPlotFlavor.class); //$NON-NLS-1$
+    xmlChartWriter.registerConverter(new CssStylesConverter());
+    xmlChartWriter.omitField(ChartLegend.class, "visible");
+    xmlChartWriter.registerConverter(new StyledTextConverter());
+    xmlChartWriter.registerConverter(new PaletteConverter());
+    xmlChartWriter.registerConverter(new ChartModelConverter());
+    xmlChartWriter.useAttributeFor(PiePlot.class, "animate"); //$NON-NLS-1$
     
-
-    chartDefWriter.setMode(XStream.NO_REFERENCES);
-    chartDefWriter.alias("ChartDataModel", ChartDataDefinition.class); //$NON-NLS-1$
+    jsonChartDefWriter.setMode(XStream.NO_REFERENCES);
+    jsonChartDefWriter.alias("chartDataModel", ChartDataDefinition.class); //$NON-NLS-1$
     
-  }
-  public static String serialize(ChartModel model){
-    return chartWriter.toXML(model);
+    xmlChartDefWriter.setMode(XStream.NO_REFERENCES);
+    xmlChartDefWriter.alias("chartDataModel", ChartDataDefinition.class); //$NON-NLS-1$
   }
   
-  public static ChartModel deSerialize(String input){
-    return (ChartModel) chartWriter.fromXML(input);
+  public static String serialize(ChartModel model, ChartSerializationFormat outputFormat){
+    String result = null;
+    switch (outputFormat) {
+      case JSON:
+        result = jsonChartWriter.toXML(model);
+        break;
+      case XML:
+        result = xmlChartWriter.toXML(model);
+        break;
+    }
+    return result;
+  }
+  
+  public static ChartModel deSerialize(String input, ChartSerializationFormat inputFormat){
+    ChartModel chartModel = null;
+    switch (inputFormat) {
+      case JSON:
+        chartModel = (ChartModel)jsonChartWriter.fromXML(input);
+        break;
+      case XML:
+        chartModel = (ChartModel)xmlChartWriter.fromXML(input);
+        break;
+    }
+    return chartModel;
   }
   
   
-  public static String serializeDataDefinition(ChartDataDefinition def){
-    return chartDefWriter.toXML(def);
-    
+  public static String serializeDataDefinition(ChartDataDefinition def, ChartSerializationFormat outputFormat){
+    String result = null;
+    switch (outputFormat) {
+      case JSON:
+        result = jsonChartDefWriter.toXML(def);
+        break;
+      case XML:
+        result = xmlChartDefWriter.toXML(def);
+        break;
+    }
+    return result;
   }
   
-  public static ChartDataDefinition deSerializeDataDefinition(String input){
-    return (ChartDataDefinition) chartDefWriter.fromXML(input);
-    
+  public static ChartDataDefinition deSerializeDataDefinition(String input, ChartSerializationFormat inputFormat){
+    ChartDataDefinition chartDataDefinition = null;
+    switch (inputFormat) {
+      case JSON:
+        chartDataDefinition = (ChartDataDefinition)jsonChartDefWriter.fromXML(input);
+        break;
+      case XML:
+        chartDataDefinition = (ChartDataDefinition)xmlChartDefWriter.fromXML(input);
+        break;
+    }
+    return chartDataDefinition;
   }
 }
--- test/org/pentaho/util/SerializationTest.java
+++ test/org/pentaho/util/SerializationTest.java
@@ -1,56 +1,106 @@
 package org.pentaho.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.pentaho.chart.model.BarPlot;
 import org.pentaho.chart.model.ChartModel;
-import org.pentaho.chart.model.StyledText;
+import org.pentaho.chart.model.Palette;
+import org.pentaho.chart.model.BarPlot.BarPlotFlavor;
+import org.pentaho.chart.model.CssStyle.FontStyle;
+import org.pentaho.chart.model.CssStyle.FontWeight;
 import org.pentaho.chart.model.Plot.Orientation;
 import org.pentaho.chart.model.Theme.ChartTheme;
 import org.pentaho.chart.model.util.ChartSerializer;
+import org.pentaho.chart.model.util.ChartSerializer.ChartSerializationFormat;
 
 public class SerializationTest {
-  private Integer color = 0x757146;
 
-  private ChartModel chartModel = new ChartModel();
-  private ChartModel model2;
-  
   @Before
-  public void init(){
-    chartModel.setTitle(new StyledText("Test Model"));
-    chartModel.setBackground(color);
-    chartModel.setTheme(ChartTheme.THEME1);
+  public void init(){}
+  
+  @Test
+  public void testBarPlot(){
+    
+    ChartModel chartModel = new ChartModel();
+    chartModel.setTheme(ChartTheme.THEME4);
+    chartModel.setBackground(0x343434);
+    chartModel.setBorderColor(0x987654);
+    chartModel.setBorderVisible(true);
+    chartModel.getTitle().setText("Chart Title");
+    chartModel.getTitle().setColor(0x123456);
+    chartModel.getTitle().setFont("monospace", 20, FontStyle.OBLIQUE, FontWeight.BOLD);
+    chartModel.getLegend().setVisible(true);
+    chartModel.getLegend().setBorderColor(0x654321);
+    chartModel.getLegend().setBorderVisible(true);
+    chartModel.getLegend().setBorderWidth(2);
+    chartModel.getLegend().setFont("verdana", 18, FontStyle.ITALIC, FontWeight.BOLD);
     
     BarPlot barPlot = new BarPlot();
+    barPlot.setBackground(0x765890);
+    barPlot.setFlavor(BarPlotFlavor.THREED);
+    barPlot.setOpacity(0.75f);
+    barPlot.setOrientation(Orientation.HORIZONTAL);
+    barPlot.setPalette(new Palette(0x001111, 0x222222, 0x333333));
+    barPlot.getXAxisLabel().setText("xAxis");
+    barPlot.getXAxisLabel().setColor(0x192837);
+    barPlot.getXAxisLabel().setFont("san-serif", 10, FontStyle.NORMAL, FontWeight.NORMAL);
+    barPlot.getYAxisLabel().setText("yAxis");
+    barPlot.getYAxisLabel().setColor(0x192837);
+    barPlot.getYAxisLabel().setFont("san-serif", 12, FontStyle.OBLIQUE, FontWeight.BOLD);
     
-    barPlot.setOrientation(Orientation.VERTICAL);
-
     chartModel.setPlot(barPlot);
     
-    String result = ChartSerializer.serialize(chartModel);
-    System.out.println(result);
-    model2 = ChartSerializer.deSerialize(result);
-  }
-  
-  @Test
-  public void testModelClass(){
     
-    assertEquals(chartModel.getTitle(), model2.getTitle());
-    assertEquals(chartModel.getBackground(), model2.getBackground());
-
-  }
-  
-  @Test
-  public void testPlot(){
-    assertEquals(chartModel.getPlot().getClass(), model2.getPlot().getClass());
-    assertEquals(((BarPlot) chartModel.getPlot()).getPalette().size()
-        ,((BarPlot) model2.getPlot()).getPalette().size());
-  }
-  
-  @Test
-  public void testTheme(){
-    assertEquals(chartModel.getTheme(), model2.getTheme());
+    String result = ChartSerializer.serialize(chartModel, ChartSerializationFormat.XML);
+    
+    System.out.println("Bar Plot");
+    System.out.println(result);
+    
+    ChartModel chartModel2 = ChartSerializer.deSerialize(result, ChartSerializationFormat.XML);
+    assertEquals(chartModel2.getTheme(), ChartTheme.THEME4);
+    assertEquals(chartModel2.getBackground(), new Integer(0x343434));
+    assertTrue(chartModel2.getBorderVisible());
+    assertEquals(chartModel2.getBorderColor(), new Integer(0x987654));
+    assertEquals(chartModel2.getBorderWidth(), new Integer(1));
+    assertEquals(chartModel2.getTitle().getText(), "Chart Title");
+    assertEquals(chartModel2.getTitle().getColor(), 0x123456);
+    assertEquals(chartModel2.getTitle().getFontFamily(), "monospace");
+    assertEquals(chartModel2.getTitle().getFontSize(), new Integer(20));
+    assertEquals(chartModel2.getTitle().getFontStyle(), FontStyle.OBLIQUE);
+    assertEquals(chartModel2.getTitle().getFontWeight(), FontWeight.BOLD);
+    assertTrue(chartModel2.getLegend().getVisible());
+    assertTrue(chartModel2.getLegend().getBorderVisible());
+    assertEquals(chartModel2.getLegend().getBorderWidth(), 2);
+    assertEquals(chartModel2.getLegend().getFontFamily(), "verdana");
+    assertEquals(chartModel2.getLegend().getFontSize(), new Integer(18));
+    assertEquals(chartModel2.getLegend().getFontStyle(), FontStyle.ITALIC);
+    assertEquals(chartModel2.getLegend().getFontWeight(), FontWeight.BOLD);
+    
+    assertTrue(chartModel2.getPlot() instanceof BarPlot);    
+    barPlot = (BarPlot)chartModel2.getPlot();
+    assertEquals(barPlot.getBackground(), new Integer(0x765890));
+    assertEquals(barPlot.getFlavor(), BarPlotFlavor.THREED);
+    assertEquals(barPlot.getOpacity(), new Float(0.75));
+    assertEquals(barPlot.getOrientation(), Orientation.HORIZONTAL);
+    Palette palette = chartModel2.getPlot().getPalette();
+    assertEquals(palette.size(), 3);
+    assertEquals(palette.get(0), 0x001111);
+    assertEquals(palette.get(1), 0x222222);
+    assertEquals(palette.get(2), 0x333333);
+    assertEquals(barPlot.getXAxisLabel().getText(), "xAxis");
+    assertEquals(barPlot.getXAxisLabel().getColor(), 0x192837);
+    assertEquals(barPlot.getXAxisLabel().getFontFamily(), "san-serif");
+    assertEquals(barPlot.getXAxisLabel().getFontSize(), 10);
+    assertEquals(barPlot.getXAxisLabel().getFontStyle(), FontStyle.NORMAL);
+    assertEquals(barPlot.getXAxisLabel().getFontWeight(), FontWeight.NORMAL);
+    assertEquals(barPlot.getYAxisLabel().getText(), "yAxis");
+    assertEquals(barPlot.getYAxisLabel().getColor(), 0x192837);
+    assertEquals(barPlot.getYAxisLabel().getFontFamily(), "san-serif");
+    assertEquals(barPlot.getYAxisLabel().getFontSize(), 12);
+    assertEquals(barPlot.getYAxisLabel().getFontStyle(), FontStyle.OBLIQUE);
+    assertEquals(barPlot.getYAxisLabel().getFontWeight(), FontWeight.BOLD);    
   }
 }
