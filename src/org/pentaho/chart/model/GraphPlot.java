ab086c5479f4c9ebad73aa2ef4ea520dafc844d4
Mat Lowery <mlowery@pentaho.com>
2009-05-14 17:31:50 +0000

    Added comment.
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-commons/pentaho-chartbeans/branches/autogendoc1@37657 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/chart/model/GraphPlot.java
+++ src/org/pentaho/chart/model/GraphPlot.java
@@ -1,21 +1,24 @@
 package org.pentaho.chart.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
+/**
+ * A plot with X and Y axes.
+ */
 public class GraphPlot extends Plot implements Serializable {
-  
+
   Number minValue = null;
+
   Number maxValue = null;
-  
+
   Axis xAxis = new Axis();
+
   Axis yAxis = new Axis();
-  
+
   public Axis getXAxis() {
     return xAxis;
   }
-  
+
   public Axis getYAxis() {
     return yAxis;
   }
