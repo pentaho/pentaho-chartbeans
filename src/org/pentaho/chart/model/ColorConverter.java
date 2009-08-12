26c854feadf64c9037a6031a289723c4ec5effac
Gretchen Moran <gmoran@pentaho.com>
2009-08-12 01:33:53 +0000

    PDB-464 - hex color strings caused this hiccup
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-commons/pentaho-chartbeans/trunk@38997 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/chart/model/ColorConverter.java
+++ src/org/pentaho/chart/model/ColorConverter.java
@@ -1,5 +1,7 @@
 package org.pentaho.chart.model;
 
+import java.awt.Color;
+
 public class ColorConverter {
 
   public static String toHexString(Object color){
@@ -10,6 +12,10 @@ public class ColorConverter {
     }
     if (color instanceof Integer){
       returnColor = Integer.toHexString(0x00FFFFFF & (Integer)color);
+    }
+    if (color instanceof Color){
+      returnColor = toHtmlColorString((Color)color);
+      return returnColor;
     }
     while (returnColor.length()<6){
       returnColor = "0".concat(returnColor);
@@ -30,7 +36,28 @@ public class ColorConverter {
       }
       returnColor = Integer.parseInt( (String)color , 16 ); 
     }
+    if (color instanceof Color){
+      returnColor = 0xFFFFFF & ((Color) color).getRGB();
+    }
     return returnColor;
   }
+  
+  public static String toHtmlColorString(Color c) {
+    if (c == null) {
+      throw new IllegalArgumentException();
+    }
+    return new StringBuilder()
+        .append('#')
+        .append( toPaddedHex(c.getRed() ) ) 
+        .append(toPaddedHex(c.getGreen() ) ) 
+        .append( toPaddedHex(c.getBlue() ) )
+        .toString();
+  }
+   
+  private static String toPaddedHex(int i) {
+    String s = Integer.toString(i & 0xff, 16); // makes it a hex string - the '& 0xff' makes it unsigned
+    return (s.length() > 1 ? s : '0' + s);
+  }
+
   
 }
---
+++ test/org/pentaho/chart/ColorConverterTest.java
@@ -0,0 +1,49 @@
+package org.pentaho.chart;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+
+import org.junit.Test;
+import org.pentaho.chart.model.ColorConverter;
+
+public class ColorConverterTest {
+
+  @Test
+  public void testToHexString() {
+    System.out.println("Color to Hex String:");
+    System.out.println("Color: " + Color.BLUE);
+    System.out.print("Hex String: ");
+    System.out.println( ColorConverter.toHexString(Color.BLUE));
+    
+    System.out.println("Integer to Hex String:");
+    System.out.println("Integer: " + 65280);
+    System.out.print("Hex String: ");
+    System.out.println( ColorConverter.toHexString(65280));
+
+    System.out.println("Integer to Hex String:");
+    System.out.println("Integer: " +  16711680);
+    System.out.print("Hex String: ");
+    System.out.println( ColorConverter.toHexString(16711680));
+    
+  }
+
+  @Test
+  public void testToInteger() {
+    System.out.println("Color to Integer:");
+    System.out.println("Color: Green");
+    System.out.print("Integer: ");
+    System.out.println(ColorConverter.toInteger(Color.GREEN));
+
+    System.out.println("Hex String to Integer:");
+    System.out.println("Hex String: #00ff00");
+    System.out.print("Integer: ");
+    System.out.println(ColorConverter.toInteger("#00ff00"));
+  }
+
+  @Test
+  public void testToHtmlColorString() {
+    fail("Not yet implemented");
+  }
+
+}
