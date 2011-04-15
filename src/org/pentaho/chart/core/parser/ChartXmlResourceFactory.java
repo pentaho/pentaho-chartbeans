3bb23926ec8e271a3f507fbd55001b05833bcd3a
Thomas Morgner <tmorgner@pentaho.com>
2011-04-15 14:16:32 +0000

    Updated ChartXml-parser to set the resource manager and key automatically. This reduces the manual work that needs to be done by the client.
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-commons/pentaho-chartbeans/trunk@45726 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/chart/core/parser/ChartXmlResourceFactory.java
+++ src/org/pentaho/chart/core/parser/ChartXmlResourceFactory.java
@@ -16,29 +16,37 @@
  */
 package org.pentaho.chart.core.parser;
 
-import org.pentaho.reporting.libraries.xmlns.parser.AbstractXmlResourceFactory;
 import org.pentaho.chart.ChartBoot;
 import org.pentaho.chart.core.ChartDocument;
 import org.pentaho.reporting.libraries.base.config.Configuration;
+import org.pentaho.reporting.libraries.resourceloader.ResourceCreationException;
+import org.pentaho.reporting.libraries.resourceloader.ResourceData;
+import org.pentaho.reporting.libraries.resourceloader.ResourceKey;
+import org.pentaho.reporting.libraries.resourceloader.ResourceLoadingException;
+import org.pentaho.reporting.libraries.resourceloader.ResourceManager;
+import org.pentaho.reporting.libraries.xmlns.parser.AbstractXmlResourceFactory;
 
 /**
  * Returns resource information used in the parsing of the chart XML document.
  *
  * @author David Kincade
  */
-public class ChartXmlResourceFactory extends AbstractXmlResourceFactory {
+public class ChartXmlResourceFactory extends AbstractXmlResourceFactory
+{
 
   /**
    * Default constructor
    */
-  public ChartXmlResourceFactory() {
+  public ChartXmlResourceFactory()
+  {
   }
 
   /**
    * Returns the configuration information that contains the
    * list of classes that will be used for parsing the chart XML file.
    */
-  protected Configuration getConfiguration() {
+  protected Configuration getConfiguration()
+  {
     return ChartBoot.getInstance().getGlobalConfig();
   }
 
@@ -46,7 +54,26 @@ public class ChartXmlResourceFactory extends AbstractXmlResourceFactory {
    * Returns the class whose fully qualified name will be used as a key
    * in the configuration file to pull out the class name of the factory module
    */
-  public Class getFactoryType() {
+  public Class getFactoryType()
+  {
     return ChartDocument.class;
+  }
+
+  protected Object finishResult(final Object res,
+                                final ResourceManager manager,
+                                final ResourceData data,
+                                final ResourceKey context) throws ResourceCreationException, ResourceLoadingException
+  {
+    final ChartDocument doc = (ChartDocument) res;
+    doc.setResourceManager(manager);
+    if (context != null)
+    {
+      doc.setResourceKey(context);
+    }
+    else
+    {
+      doc.setResourceKey(data.getKey());
+    }
+    return doc;
   }
 }
