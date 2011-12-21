babf87c77f32cff05dd92ffbd538ad8a47547ea2
Nick Baker <nbaker@pentaho.com>
2011-12-21 12:16:52 +0000

    Removing static call to set JFreeChart Theme now that it's done inside of the reporting engine.
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-commons/pentaho-chartbeans/trunk@48887 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/chart/plugin/jfreechart/JFreeChartFactoryEngine.java
+++ src/org/pentaho/chart/plugin/jfreechart/JFreeChartFactoryEngine.java
@@ -30,7 +30,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.StandardChartTheme;
 import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.axis.CategoryLabelPositions;
 import org.jfree.chart.axis.NumberAxis;
@@ -124,10 +123,6 @@ public class JFreeChartFactoryEngine implements Serializable {
   
   private static final long serialVersionUID = -1079376910255750394L;
 
-  static {
-	  ChartFactory.setChartTheme(StandardChartTheme.createLegacyTheme());
-  }
-  
   public JFreeChartFactoryEngine(){
   }
 
