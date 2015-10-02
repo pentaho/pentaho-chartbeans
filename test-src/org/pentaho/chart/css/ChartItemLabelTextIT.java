0983b7d1b5b5f808b57ba7bab0081e513756b493
Diogo Mariano <dmariano@pentaho.com>
2015-10-02 18:11:14 +0100

    [BACKLOG-5407] - Classify Unit Test as Integration Tests. Tests with file system access marked as Integration Test - Standardized test dir, removing test directory override, allowing Integration Tests to run properly - Re-enabled and fixed ChartItemLabelGeneratorIT test

---
+++ test-src/org/pentaho/chart/css/ChartItemLabelTextIT.java
@@ -0,0 +1,72 @@
+/*!
+* This program is free software; you can redistribute it and/or modify it under the
+* terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+* Foundation.
+*
+* You should have received a copy of the GNU Lesser General Public License along with this
+* program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+* or from the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU Lesser General Public License for more details.
+*
+* Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+*/
+
+package org.pentaho.chart.css;
+
+import junit.framework.TestCase;
+
+import org.pentaho.chart.ChartBoot;
+import org.pentaho.chart.ChartDocumentContext;
+import org.pentaho.chart.ChartFactory;
+import org.pentaho.chart.core.ChartDocument;
+import org.pentaho.chart.core.ChartElement;
+import org.pentaho.chart.css.keys.ChartStyleKeys;
+import org.pentaho.reporting.libraries.css.dom.LayoutStyle;
+import org.pentaho.reporting.libraries.css.values.CSSStringType;
+import org.pentaho.reporting.libraries.css.values.CSSStringValue;
+
+public class ChartItemLabelTextIT extends TestCase {
+
+  @Override
+  protected void setUp() throws Exception {    
+    super.setUp();
+    
+    // Boot the charting library - required for parsing configuration
+    ChartBoot.getInstance().start();
+  }
+  
+  public void testChartLabelText() throws Exception {
+    final ChartDocumentContext cdc = ChartFactory.generateChart(getClass().getResource("ChartItemLabelTextTest.xml")); //$NON-NLS-1$
+    final ChartDocument cd = cdc.getChartDocument();
+    assertNotNull(cd);
+    final ChartElement element = cd.getRootElement();
+    assertNotNull(element);
+
+    final CSSStringValue[] passValues = new CSSStringValue[] {
+      new CSSStringValue(CSSStringType.STRING, "{0,$###,###.##} this is a label {1, format}"), //$NON-NLS-1$  
+      new CSSStringValue(CSSStringType.STRING, "{0}"), //$NON-NLS-1$
+      new CSSStringValue(CSSStringType.STRING, ""), //$NON-NLS-1$
+      new CSSStringValue(CSSStringType.STRING, "{0}") //$NON-NLS-1$
+    };
+    
+    int counter = 0;
+    final int lenArray = passValues.length;
+    ChartElement child = element.getFirstChildItem();
+    
+    while(child != null) {
+      final LayoutStyle layoutStyle = child.getLayoutStyle();
+      assertNotNull(layoutStyle);
+      System.out.println("Expected: "+passValues[counter]+" - Got: "+layoutStyle.getValue(ChartStyleKeys.ITEM_LABEL_TEXT).getCSSText()); //$NON-NLS-1$ //$NON-NLS-2$
+      assertEquals(passValues[counter++].getCSSText(), layoutStyle.getValue(ChartStyleKeys.ITEM_LABEL_TEXT).getCSSText());
+      child = child.getNextItem();
+    }
+
+    if (counter < lenArray-1) {
+      throw new IllegalStateException("Not all tests covered!");  //$NON-NLS-1$
+    }
+  }
+}
---
+++ test-src/org/pentaho/chart/css/ChartMarkerFillColorIT.java
@@ -0,0 +1,74 @@
+/*!
+* This program is free software; you can redistribute it and/or modify it under the
+* terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+* Foundation.
+*
+* You should have received a copy of the GNU Lesser General Public License along with this
+* program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+* or from the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU Lesser General Public License for more details.
+*
+* Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+*/
+
+package org.pentaho.chart.css;
+
+import java.awt.Color;
+
+import junit.framework.TestCase;
+
+import org.pentaho.chart.ChartBoot;
+import org.pentaho.chart.ChartDocumentContext;
+import org.pentaho.chart.ChartFactory;
+import org.pentaho.chart.core.ChartDocument;
+import org.pentaho.chart.core.ChartElement;
+import org.pentaho.chart.css.keys.ChartStyleKeys;
+import org.pentaho.reporting.libraries.css.dom.LayoutStyle;
+import org.pentaho.reporting.libraries.css.values.CSSColorValue;
+
+public class ChartMarkerFillColorIT extends TestCase {
+
+  @Override
+  protected void setUp() throws Exception {    
+    super.setUp();
+    
+    // Boot the charting library - required for parsing configuration
+    ChartBoot.getInstance().start();
+  }
+  
+  public void testMarkerFillColorStyle() throws Exception {
+    final ChartDocumentContext cdc = ChartFactory.generateChart(getClass().getResource("ChartMarkerFillColorTest.xml")); //$NON-NLS-1$
+    final ChartDocument cd = cdc.getChartDocument();
+    assertNotNull(cd);
+    final ChartElement element = cd.getRootElement();
+    assertNotNull(element);
+
+    final CSSColorValue[] passValues = new CSSColorValue[]{
+        new CSSColorValue(Color.WHITE),
+        new CSSColorValue(Color.BLACK),
+        new CSSColorValue(Color.RED),
+    };
+    
+    int counter = 0;
+    final int lenArray = passValues.length;
+    ChartElement child = element.getFirstChildItem();
+    
+    while(child != null) {
+      final LayoutStyle layoutStyle = child.getLayoutStyle();
+      assertNotNull(layoutStyle);
+      System.out.println("Expected: "+passValues[counter]+" - Got: "+layoutStyle.getValue(ChartStyleKeys.MARKER_FILL_COLOR)); //$NON-NLS-1$ //$NON-NLS-2$
+      assertEquals(passValues[counter++], layoutStyle.getValue(ChartStyleKeys.MARKER_FILL_COLOR));
+      child = child.getNextItem();
+    }
+
+    if (counter < lenArray-1) {
+      throw new IllegalStateException("Not all tests covered!");  //$NON-NLS-1$
+    }
+  }
+  
+  
+}
