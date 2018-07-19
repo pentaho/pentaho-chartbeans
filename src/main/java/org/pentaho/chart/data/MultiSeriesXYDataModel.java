f0e335d7460280857552958339c4991f922189f1
Luis Martins <33635091+pentaho-lmartins@users.noreply.github.com>
2018-07-19 18:18:03 +0100

    Revert "[SP-4333] Backport of PDB-2003 - Dashboard Content Linking with Date …"

--- src/main/java/org/pentaho/chart/data/MultiSeriesXYDataModel.java
+++ src/main/java/org/pentaho/chart/data/MultiSeriesXYDataModel.java
@@ -1,19 +1,19 @@
 /*!
- * This program is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
- * Foundation.
- *
- * You should have received a copy of the GNU Lesser General Public License along with this
- * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
- * or from the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
- *
- * Copyright (c) 2002-2018 Hitachi Vantara..  All rights reserved.
- */
+* This program is free software; you can redistribute it and/or modify it under the
+* terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+* Foundation.
+*
+* You should have received a copy of the GNU Lesser General Public License along with this
+* program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+* or from the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU Lesser General Public License for more details.
+*
+* Copyright (c) 2002-2017 Hitachi Vantara..  All rights reserved.
+*/
 
 package org.pentaho.chart.data;
 
@@ -23,83 +23,61 @@ import java.util.List;
 import java.util.Map;
 
 public class MultiSeriesXYDataModel implements IChartDataModel {
-
-  LinkedHashMap<FormattableName, Series> chartData = new LinkedHashMap<>();
-
+  
   public class Series extends XYDataModel {
-    FormattableName seriesName;
-
+    String seriesName;
+    
     Series() {
     }
-
-    Series( FormattableName seriesName ) {
+    
+    Series(String seriesName) {
       this.seriesName = seriesName;
     }
-
-    Series( String seriesKey ) {
-      this.seriesName = new FormattableName( seriesKey );
-    }
-
-    public FormattableName getSeriesNameInstance() {
+    
+    public String getSeriesName() {
       return seriesName;
     }
 
-    public String getSeriesName() {
-      return seriesName.getKey();
-    }
-
-    public String getSeriesFormattedName() {
-      return seriesName.getFormatted();
-    }
-
-    public void setSeriesName( String seriesKey ) {
-      this.seriesName.setKey( seriesKey );
-    }
-
-    public void setSeriesFormattedName( String seriesFormattedName ) {
-      this.seriesName.setFormatted( seriesFormattedName );
+    public void setSeriesName(String seriesName) {
+      this.seriesName = seriesName;
     }
   }
-
+  
+  LinkedHashMap<String, XYDataModel> chartData = new LinkedHashMap<String, XYDataModel>();
+  
   public MultiSeriesXYDataModel() {
   }
-
-  public void addDataPoint( String seriesKey, Number domainValue, Number rangeValue ) {
-    addDataPoint( seriesKey, null, domainValue, rangeValue );
-  }
-
-  public void addDataPoint( String seriesKey, String seriesFormatted, Number domainValue, Number rangeValue ) {
-
-    FormattableName seriesName = new FormattableName( seriesKey, seriesFormatted );
-    Series seriesData = chartData.get( seriesName );
-
-    if ( seriesData == null ) {
-      seriesData = new Series( seriesName );
-      chartData.put( seriesName, seriesData );
+ 
+  public void addDataPoint(String seriesName, Number domainValue, Number rangeValue) {    
+    XYDataModel xyDataModel = chartData.get(seriesName);    
+    
+    if (xyDataModel == null) {
+      xyDataModel = new XYDataModel();
+      chartData.put(seriesName, xyDataModel);
     }
-
-    seriesData.add( new XYDataPoint( domainValue, rangeValue ) );
+    
+    xyDataModel.add(new XYDataPoint(domainValue, rangeValue));    
   }
-
-  public Series getSeries( String seriesKey ) {
-    Series seriesData = null;
-    Series existingSeriesData = chartData.get( new FormattableName( seriesKey ) );
-    if ( existingSeriesData != null ) {
-      seriesData = new Series( existingSeriesData.seriesName );
-      seriesData.addAll( existingSeriesData );
+  
+  public Series getSeries(String seriesName) {
+    Series series = null;
+    XYDataModel xyDataModel = chartData.get(seriesName);
+    if (xyDataModel != null) {
+      series = new Series(seriesName);
+      series.addAll(xyDataModel);
     }
-    return seriesData;
+    return series;
   }
-
+  
   public List<Series> getSeries() {
-    List<Series> seriesDataList = new ArrayList<>();
-
-    for ( Map.Entry<FormattableName, Series> mapEntry : chartData.entrySet() ) {
-      Series series = new Series( mapEntry.getKey() );
-      series.addAll( mapEntry.getValue() );
-      seriesDataList.add( series );
+    List<Series> seriesList = new ArrayList<Series>();
+    
+    for (Map.Entry<String, XYDataModel> mapEntry : chartData.entrySet()) {
+      Series series = new Series(mapEntry.getKey());
+      series.addAll(mapEntry.getValue());      
+      seriesList.add(series);
     }
-
-    return seriesDataList;
+    
+    return seriesList;
   }
 }
--- src/test/java/org/pentaho/chart/ChartBeanFactoryTest.java
+++ src/test/java/org/pentaho/chart/ChartBeanFactoryTest.java
@@ -2,7 +2,7 @@
  *
  * Pentaho Data Integration
  *
- * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2002-2017 by Hitachi Vantara : http://www.pentaho.com
  *
  *******************************************************************************
  *
@@ -28,291 +28,93 @@ import static org.mockito.Mockito.when;
 import static org.junit.Assert.assertEquals;
 
 import java.text.SimpleDateFormat;
-import java.text.DecimalFormat;
 import java.util.Date;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.pentaho.chart.data.MultiSeriesDataModel;
-import org.pentaho.chart.data.MultiSeriesXYDataModel;
 import org.pentaho.chart.model.ChartModel;
-import org.pentaho.chart.model.ScatterPlot;
 import org.pentaho.chart.plugin.ChartDataOverflowException;
 import org.pentaho.chart.plugin.NoChartDataException;
-import org.pentaho.chart.plugin.jfreechart.JFreeChartPlugin;
 import org.pentaho.commons.connection.IPentahoMetaData;
 import org.pentaho.metadata.model.concept.types.DataType;
 
 public class ChartBeanFactoryTest {
 
+  private static final int ARG_NAME_INDEX = 2;
+
   private static final String ARG_NAME_MASK = "mask";
 
   private static final String ARG_NAME_TYPE = "datatype";
 
-  private static final String SAMPLE_INTEGER_MASK = "###,###";
-
-  private static final String SAMPLE_DATE_MASK = "yyyy-MM-dd";
-
-  private static final int RANGE = 1;
-
-  private static final int RANGE_COLUMN_INDEX = 2;
-
-  private static final int SERIES_COLUMN_INDEX = 1;
-
-  private static final int DOMAIN_COLUMN_INDEX = 0;
-
-  private static final int ARG_NAME_INDEX = 2;
-
   private static final int SAMPLE_INTEGER = 123456;
 
-  private static final Number SCALING_FACTOR = 1;
+  private static final String SAMPLE_INTEGER_MASK = "###,###";
 
-  private static final boolean CONVERTS_NULL_TO_ZERO = false;
-
-  private Object[][] queryResults;
-
-  private String expectedDate;
-
-  private String expectedInteger;
+  private static final String EXPECTED_INTEGER = "123,456";
 
   private Date sampleDate;
 
-  private SimpleDateFormat format;
+  private static final String SAMPLE_DATE_MASK = "yyyy-MM-dd";
 
-  private ChartModel chartModel;
+  private String expectedDate;
 
-  private IPentahoMetaData metadata;
-
-  @Before
-  public void initialize() {
-
-    chartModel = new ChartModel();
+  @Test
+  public void testCreateChartDataModel_MultiSeriesDataModel_Date() throws ChartDataOverflowException,
+    NoChartDataException {
     sampleDate = new Date( 0 );
-    format = new SimpleDateFormat( SAMPLE_DATE_MASK );
+    SimpleDateFormat format = new SimpleDateFormat( SAMPLE_DATE_MASK );
     expectedDate = format.format( sampleDate );
-    queryResults = new Object[][] { { "domain", sampleDate, RANGE } };
-    metadata = mock( IPentahoMetaData.class );
 
+    IPentahoMetaData metadata = mock( IPentahoMetaData.class );
     when( metadata.getAttribute( anyInt(), anyInt(), anyString() ) ).then( new Answer<Object>() {
       @Override
       public Object answer( InvocationOnMock invocation ) throws Throwable {
-        if ( ARG_NAME_MASK.equalsIgnoreCase( (String) invocation.getArguments()[ ARG_NAME_INDEX ] ) ) {
+        if ( ARG_NAME_MASK.equalsIgnoreCase( (String) invocation.getArguments()[ARG_NAME_INDEX] ) ) {
           return SAMPLE_DATE_MASK;
-        } else if ( ARG_NAME_TYPE.equalsIgnoreCase( (String) invocation.getArguments()[ ARG_NAME_INDEX ] ) ) {
+        } else if ( ARG_NAME_TYPE.equalsIgnoreCase( (String) invocation.getArguments()[ARG_NAME_INDEX] ) ) {
           return DataType.DATE;
         }
-        return (String) invocation.getArguments()[ ARG_NAME_INDEX ];
+        return (String) invocation.getArguments()[ARG_NAME_INDEX];
       }
     } );
-
+    testCreateChartDataModel_MultiSeriesDataModel( expectedDate, sampleDate, metadata );
   }
 
   @Test
-  public void testCreateChartDataModelScatterPlot_Date_OpenFlash()
-    throws ChartDataOverflowException, NoChartDataException {
-
-    ScatterPlot scatterPlot = new ScatterPlot();
-    chartModel.setPlot( scatterPlot );
-
-    MultiSeriesXYDataModel model =
-      (MultiSeriesXYDataModel) ChartBeanFactory
-        .createChartDataModel( queryResults, SCALING_FACTOR, CONVERTS_NULL_TO_ZERO,
-          RANGE_COLUMN_INDEX, SERIES_COLUMN_INDEX, DOMAIN_COLUMN_INDEX, chartModel, metadata );
-
-    assertEquals( expectedDate, model.getSeries().get( DOMAIN_COLUMN_INDEX ).getSeriesFormattedName() );
-
-    metadata = null;
-
-    MultiSeriesXYDataModel modelWithNullMask =
-      (MultiSeriesXYDataModel) ChartBeanFactory
-        .createChartDataModel( queryResults, SCALING_FACTOR, CONVERTS_NULL_TO_ZERO,
-          RANGE_COLUMN_INDEX, SERIES_COLUMN_INDEX, DOMAIN_COLUMN_INDEX, chartModel, metadata );
-
-    assertEquals( sampleDate.toString(),
-      modelWithNullMask.getSeries().get( DOMAIN_COLUMN_INDEX ).getSeriesFormattedName() );
-
-  }
-
-  @Test
-  public void testCreateChartDataModelScatterPlot_Date_NullMask_OpenFlash()
-    throws ChartDataOverflowException, NoChartDataException {
-
-    ScatterPlot scatterPlot = new ScatterPlot();
-    chartModel.setPlot( scatterPlot );
-
-    metadata = null;
-
-    MultiSeriesXYDataModel modelWithNullMask =
-      (MultiSeriesXYDataModel) ChartBeanFactory
-        .createChartDataModel( queryResults, SCALING_FACTOR, CONVERTS_NULL_TO_ZERO,
-          RANGE_COLUMN_INDEX, SERIES_COLUMN_INDEX, DOMAIN_COLUMN_INDEX, chartModel, metadata );
-
-    assertEquals( sampleDate.toString(),
-      modelWithNullMask.getSeries().get( DOMAIN_COLUMN_INDEX ).getSeriesFormattedName() );
-
-  }
-
-  @Test
-  public void testCreateChartDataModel_MultiSeriesDataModel_Date_OpenFlash() throws ChartDataOverflowException,
+  public void testCreateChartDataModel_MultiSeriesDataModel_Numeric() throws ChartDataOverflowException,
     NoChartDataException {
-
-    MultiSeriesDataModel model =
-      (MultiSeriesDataModel) ChartBeanFactory
-        .createChartDataModel( queryResults, SCALING_FACTOR, CONVERTS_NULL_TO_ZERO,
-          RANGE_COLUMN_INDEX, SERIES_COLUMN_INDEX, DOMAIN_COLUMN_INDEX, chartModel, metadata );
-
-    assertEquals( expectedDate, model.getSeriesData().get( DOMAIN_COLUMN_INDEX ).getSeriesFormattedName() );
-
-    metadata = null;
-
-    MultiSeriesDataModel modelWithNullMask =
-      (MultiSeriesDataModel) ChartBeanFactory
-        .createChartDataModel( queryResults, SCALING_FACTOR, CONVERTS_NULL_TO_ZERO,
-          RANGE_COLUMN_INDEX, SERIES_COLUMN_INDEX, DOMAIN_COLUMN_INDEX, chartModel, metadata );
-
-    assertEquals( sampleDate.toString(),
-      modelWithNullMask.getSeriesData().get( DOMAIN_COLUMN_INDEX ).getSeriesFormattedName() );
-
-  }
-
-  @Test
-  public void testCreateChartDataModel_MultiSeriesDataModel_Date_NullMask_OpenFlash() throws ChartDataOverflowException,
-    NoChartDataException {
-
-    metadata = null;
-
-    MultiSeriesDataModel modelWithNullMask =
-      (MultiSeriesDataModel) ChartBeanFactory
-        .createChartDataModel( queryResults, SCALING_FACTOR, CONVERTS_NULL_TO_ZERO,
-          RANGE_COLUMN_INDEX, SERIES_COLUMN_INDEX, DOMAIN_COLUMN_INDEX, chartModel, metadata );
-
-    assertEquals( sampleDate.toString(),
-      modelWithNullMask.getSeriesData().get( DOMAIN_COLUMN_INDEX ).getSeriesFormattedName() );
-
-  }
-
-  @Test
-  public void testCreateChartDataModel_MultiSeriesDataModel_Numeric_OpenFlash() throws ChartDataOverflowException,
-    NoChartDataException {
-
-    DecimalFormat decimalFormat = new DecimalFormat( SAMPLE_INTEGER_MASK );
-    expectedInteger = decimalFormat.format( SAMPLE_INTEGER );
-    queryResults = new Object[][] { { "domain", SAMPLE_INTEGER, RANGE } };
-
+    IPentahoMetaData metadata = mock( IPentahoMetaData.class );
     when( metadata.getAttribute( anyInt(), anyInt(), anyString() ) ).then( new Answer<Object>() {
       @Override
       public Object answer( InvocationOnMock invocation ) throws Throwable {
-        if ( ARG_NAME_MASK.equalsIgnoreCase( (String) invocation.getArguments()[ ARG_NAME_INDEX ] ) ) {
+        if ( ARG_NAME_MASK.equalsIgnoreCase( (String) invocation.getArguments()[ARG_NAME_INDEX] ) ) {
           return SAMPLE_INTEGER_MASK;
-        } else if ( ARG_NAME_TYPE.equalsIgnoreCase( (String) invocation.getArguments()[ ARG_NAME_INDEX ] ) ) {
+        } else if ( ARG_NAME_TYPE.equalsIgnoreCase( (String) invocation.getArguments()[ARG_NAME_INDEX] ) ) {
           return DataType.NUMERIC;
         }
-        return (String) invocation.getArguments()[ ARG_NAME_INDEX ];
+        return (String) invocation.getArguments()[ARG_NAME_INDEX];
       }
     } );
+    testCreateChartDataModel_MultiSeriesDataModel( EXPECTED_INTEGER, SAMPLE_INTEGER, metadata );
+  }
+
+  public void testCreateChartDataModel_MultiSeriesDataModel( String expected, Object sample, IPentahoMetaData metadata )
+    throws ChartDataOverflowException, NoChartDataException {
+    int range = 1;
+    Object[][] queryResults = new Object[][] { { "domain", sample, range } };
+    Number scalingFactor = 1;
+    boolean convertNullsToZero = false;
+    int rangeColumnIndex = 2;
+    int seriesColumnIdx = 1;
+    int domainColumnIdx = 0;
+    ChartModel chartModel = mock( ChartModel.class );
 
     MultiSeriesDataModel model =
-      (MultiSeriesDataModel) ChartBeanFactory
-        .createChartDataModel( queryResults, SCALING_FACTOR, CONVERTS_NULL_TO_ZERO,
-          RANGE_COLUMN_INDEX, SERIES_COLUMN_INDEX, DOMAIN_COLUMN_INDEX, chartModel, metadata );
-
-    assertEquals( expectedInteger, model.getSeriesData().get( DOMAIN_COLUMN_INDEX ).getSeriesFormattedName() );
-
-    metadata = null;
-
-    MultiSeriesDataModel modelWithNullMask =
-      (MultiSeriesDataModel) ChartBeanFactory
-        .createChartDataModel( queryResults, SCALING_FACTOR, CONVERTS_NULL_TO_ZERO,
-          RANGE_COLUMN_INDEX, SERIES_COLUMN_INDEX, DOMAIN_COLUMN_INDEX, chartModel, metadata );
-
-    assertEquals( Integer.toString( SAMPLE_INTEGER ),
-      modelWithNullMask.getSeriesData().get( DOMAIN_COLUMN_INDEX ).getSeriesFormattedName() );
-
-  }
-
-  @Test
-  public void testCreateChartDataModel_MultiSeriesDataModel_Numeric_NullMask_OpenFlash()
-    throws ChartDataOverflowException,
-    NoChartDataException {
-
-    DecimalFormat decimalFormat = new DecimalFormat( SAMPLE_INTEGER_MASK );
-    expectedInteger = decimalFormat.format( SAMPLE_INTEGER );
-    queryResults = new Object[][] { { "domain", SAMPLE_INTEGER, RANGE } };
-
-    metadata = null;
-
-    MultiSeriesDataModel modelWithNullMask =
-      (MultiSeriesDataModel) ChartBeanFactory
-        .createChartDataModel( queryResults, SCALING_FACTOR, CONVERTS_NULL_TO_ZERO,
-          RANGE_COLUMN_INDEX, SERIES_COLUMN_INDEX, DOMAIN_COLUMN_INDEX, chartModel, metadata );
-
-    assertEquals( Integer.toString( SAMPLE_INTEGER ),
-      modelWithNullMask.getSeriesData().get( DOMAIN_COLUMN_INDEX ).getSeriesFormattedName() );
-
-  }
-
-  @Test
-  public void testCreateChartDataModelScatterPlot_Date_JFree() throws ChartDataOverflowException, NoChartDataException {
-
-    chartModel.setChartEngineId( JFreeChartPlugin.PLUGIN_ID );
-    ScatterPlot scatterPlot = new ScatterPlot();
-    chartModel.setPlot( scatterPlot );
-
-    MultiSeriesXYDataModel model =
-      (MultiSeriesXYDataModel) ChartBeanFactory
-        .createChartDataModel( queryResults, SCALING_FACTOR, CONVERTS_NULL_TO_ZERO,
-          RANGE_COLUMN_INDEX, SERIES_COLUMN_INDEX, DOMAIN_COLUMN_INDEX, chartModel, metadata );
-
-    assertEquals( expectedDate, model.getSeries().get( DOMAIN_COLUMN_INDEX ).getSeriesFormattedName() );
-
-  }
-
-  @Test
-  public void testCreateChartDataModel_MultiSeriesDataModel_Date_JFree() throws ChartDataOverflowException,
-    NoChartDataException {
-
-    chartModel.setChartEngineId( JFreeChartPlugin.PLUGIN_ID );
-
-    MultiSeriesDataModel model =
-      (MultiSeriesDataModel) ChartBeanFactory
-        .createChartDataModel( queryResults, SCALING_FACTOR, CONVERTS_NULL_TO_ZERO,
-          RANGE_COLUMN_INDEX, SERIES_COLUMN_INDEX, DOMAIN_COLUMN_INDEX, chartModel, metadata );
-
-    assertEquals( expectedDate, model.getSeriesData().get( DOMAIN_COLUMN_INDEX ).getSeriesFormattedName() );
-
-  }
-
-  @Test
-  public void testCreateChartDataModel_MultiSeriesDataModel_Numeric_JFree() throws ChartDataOverflowException,
-    NoChartDataException {
-
-    chartModel.setChartEngineId( JFreeChartPlugin.PLUGIN_ID );
-
-    DecimalFormat decimalFormat = new DecimalFormat( SAMPLE_INTEGER_MASK );
-    expectedInteger = decimalFormat.format( SAMPLE_INTEGER );
-    queryResults = new Object[][] { { "domain", SAMPLE_INTEGER, RANGE } };
-
-    when( metadata.getAttribute( anyInt(), anyInt(), anyString() ) ).then( new Answer<Object>() {
-      @Override
-      public Object answer( InvocationOnMock invocation ) throws Throwable {
-        if ( ARG_NAME_MASK.equalsIgnoreCase( (String) invocation.getArguments()[ ARG_NAME_INDEX ] ) ) {
-          return SAMPLE_INTEGER_MASK;
-        } else if ( ARG_NAME_TYPE.equalsIgnoreCase( (String) invocation.getArguments()[ ARG_NAME_INDEX ] ) ) {
-          return DataType.NUMERIC;
-        }
-        return (String) invocation.getArguments()[ ARG_NAME_INDEX ];
-      }
-    } );
-
-    MultiSeriesDataModel model =
-      (MultiSeriesDataModel) ChartBeanFactory
-        .createChartDataModel( queryResults, SCALING_FACTOR, CONVERTS_NULL_TO_ZERO,
-          RANGE_COLUMN_INDEX, SERIES_COLUMN_INDEX, DOMAIN_COLUMN_INDEX, chartModel, metadata );
-
-    assertEquals( expectedInteger, model.getSeriesData().get( DOMAIN_COLUMN_INDEX ).getSeriesFormattedName() );
-
+        (MultiSeriesDataModel) ChartBeanFactory.createChartDataModel( queryResults, scalingFactor, convertNullsToZero,
+            rangeColumnIndex, seriesColumnIdx, domainColumnIdx, chartModel, metadata );
+    assertEquals( expected, model.getSeriesData( expected ).getSeriesName() );
   }
 
 }
